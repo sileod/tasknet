dd67f2ee41654fbac622d4cca6e4b4b81dc265c6
Damien Sileo <dsileo@magnet-magnet6.lille.inria.fr>
2022-11-07 14:10:44 +0100

    .

--- src/models.py
+++ src/models.py
@@ -1,1 +1,344 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import transformers
+import datasets
+from datasets import load_dataset
+from torch.utils.data.dataloader import DataLoader
+from transformers.data.data_collator import InputDataClass
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data.sampler import RandomSampler
+from typing import List, Union, Dict
+from transformers import DefaultDataCollator
+from transformers import DataCollatorForTokenClassification
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    DataCollatorForSeq2Seq,
+    AutoModelForSequenceClassification,
+    AutoModelForMultipleChoice,
+    AutoModelForTokenClassification,
+)
+from transformers import EncoderDecoderModel
+from evaluate import load as load_metric
+from lazy_load import lazy_func
+from easydict import EasyDict as edict
+import funcy as fc
+import evaluate
+import copy
+import inspect
+import functools
+from dataclasses import dataclass
+from types import MappingProxyType
+from tasks import Classification
 
+load_dataset = lazy_func(datasets.load_dataset)
+
+
+def load_Seq2Seq(model_name, tokenizer):
+    model = EncoderDecoderModel.from_encoder_decoder_pretrained(
+        model_name, model_name, tie_encoder_decoder=True
+    )
+
+    model.config.decoder_start_token_id = tokenizer.cls_token_id
+    model.config.eos_token_id = tokenizer.sep_token_id
+    model.config.pad_token_id = tokenizer.pad_token_id
+    model.config.vocab_size = model.config.encoder.vocab_size
+    return model
+
+
+class CLSEmbedding(nn.Module):
+    def __init__(self, Zi):
+        super().__init__()
+        self.cls = Zi
+
+    def forward(self, x):
+        x[:, 0, :] = x[:, 0, :] + self.cls
+        return x
+
+
+class Model(transformers.PreTrainedModel):
+    def __init__(self, tasks, args):
+        super().__init__(transformers.PretrainedConfig())
+        tasks=[x(tokenizer=edict()) if inspect.isclass(x) else x for x in tasks]
+        self.Z = nn.parameter.Parameter(torch.zeros(len(tasks), 768, device="cuda"))
+        shared_encoder = None
+        task_models_list = []
+        for i, task in enumerate(tasks):
+            model_type = eval(f"AutoModelFor{task.task_type}")
+            nl = (
+                {"num_labels": getattr(task, "num_labels")}
+                if hasattr(task, "num_labels")
+                else {}
+            )
+            model = model_type.from_pretrained(args.model_name, **nl)
+            model.i = i
+            if shared_encoder is None:
+                # shared_encoder = getattr(model, cls.get_encoder_attr_name(model))
+                shared_encoder = fc.first(model.children())
+            else:
+                # setattr(model, self.get_encoder_attr_name(model), copy.copy(shared_encoder))
+                self.shallow_copy(
+                    shared_encoder, getattr(model, self.get_encoder_attr_name(model))
+                )
+            task_models_list += [model]
+        self.task_models_list = nn.ModuleList(task_models_list)
+
+        for i, task in enumerate(tasks):
+            self.task_models_list[i].roberta.embeddings.word_embeddings = nn.Sequential(
+                self.task_models_list[i].roberta.embeddings.word_embeddings,
+                CLSEmbedding(self.Z[i]),
+            )
+
+    @staticmethod
+    def shallow_copy(A, B):
+        """A into B
+        https://stackoverflow.com/questions/31174295/getattr-and-setattr-on-nested-objects/31174427?noredirect=1#comment86638618_31174427"""
+
+        def rsetattr(obj, attr, val):
+            pre, _, post = attr.rpartition(".")
+            return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+
+        def rgetattr(obj, attr, *args):
+            def _getattr(obj, attr):
+                return getattr(obj, attr, *args)
+
+            return functools.reduce(_getattr, [obj] + attr.split("."))
+
+        for (na, pa), (nb, pb) in zip(A.named_parameters(), B.named_parameters()):
+            rsetattr(B, nb, rgetattr(A, na))
+        return A, B
+
+    @classmethod
+    def get_encoder_attr_name(cls, model):
+        if hasattr(model, "encoder"):
+            return "encoder"
+        else:
+            return model.config.model_type
+
+    def forward(self, task, **kwargs):
+        task_index = task[0].item()
+        y = self.task_models_list[task_index](**kwargs)
+        return y
+
+
+class NLPDataCollator:
+    def __init__(self, tasks):
+        self.tasks = tasks
+
+    def __call__(
+        self, features: List[Union[InputDataClass, Dict]]
+    ) -> Dict[str, torch.Tensor]:
+        task_index = features[0]["task"]
+        return self.tasks[task_index].data_collator.__call__(features)
+
+
+class DataLoaderWithTaskname:
+    def __init__(self, task_name, data_loader):
+        self.task = task_name
+        self.data_loader = data_loader
+        self.batch_size = data_loader.batch_size
+        self.dataset = data_loader.dataset
+
+    def __len__(self):
+        return len(self.data_loader)
+
+    def __iter__(self):
+        for batch in self.data_loader:
+            yield batch
+
+
+class MultitaskDataloader:
+    """
+    Data loader that combines and samples from multiple single-task
+    data loaders.
+    """
+
+    def __init__(self, dataloader_dict):
+        self.dataloader_dict = dataloader_dict
+        self.num_batches_dict = {
+            task_name: len(dataloader)
+            for task_name, dataloader in self.dataloader_dict.items()
+        }
+        self.task_name_list = list(self.dataloader_dict)
+        self.dataset = [None] * sum(
+            len(dataloader.dataset) for dataloader in self.dataloader_dict.values()
+        )
+
+    def __len__(self):
+        return sum(self.num_batches_dict.values())
+
+    def __iter__(self):
+        """
+        For each batch, sample a task, and yield a batch from the respective
+        task Dataloader.
+
+        We use size-proportional sampling, but you could easily modify this
+        to sample from some-other distribution.
+        """
+        task_choice_list = []
+        for i, task_name in enumerate(self.task_name_list):
+            task_choice_list += [i] * self.num_batches_dict[task_name]
+        task_choice_list = np.array(task_choice_list)
+        np.random.shuffle(task_choice_list)
+        dataloader_iter_dict = {
+            task_name: iter(dataloader)
+            for task_name, dataloader in self.dataloader_dict.items()
+        }
+        for task_choice in task_choice_list:
+            task_name = self.task_name_list[task_choice]
+            yield next(dataloader_iter_dict[task_name])
+
+
+class Trainer(transformers.Trainer):
+    def __init__(self, model, tokenizer, tasks, hparams, *args, **kwargs):
+        class default:
+            output_dir = "./models/multitask_model"
+            evaluation_strategy = "epoch"
+            overwrite_output_dir = True
+            do_train = True
+            per_device_train_batch_size = 8
+            save_steps = 1000000
+            label_names = ["labels"]
+            include_inputs_for_metrics = True
+
+        default = {
+            k: v for (k, v) in default.__dict__.items() if not k.startswith("__")
+        }
+        hparams = {
+            k: v for (k, v) in hparams.__dict__.items() if not k.startswith("__")
+        }
+
+        trainer_args = transformers.TrainingArguments(
+            **{**default, **fc.project(hparams, dir(transformers.TrainingArguments))},
+        )
+
+        super().__init__(
+            model,
+            trainer_args,
+            compute_metrics=Classification.compute_metrics,
+            *args,
+            **kwargs,
+        )
+        tasks=[x(tokenizer=tokenizer) if inspect.isclass(x) else x for x in tasks]
+        self.data_collator = NLPDataCollator(tasks)
+        self.tasks = tasks
+        for t in tasks:
+            if not hasattr(t, "name") or not t.name:
+                t.name = t.__class__.__name__
+        self.tokenizer = tokenizer
+        self.processed_tasks = preprocess_tasks(tasks, self.tokenizer)
+        self.train_dataset = {
+            task: dataset["train"] for task, dataset in self.processed_tasks.items()
+        }
+        self.eval_dataset = {
+            task: dataset["validation"]
+            for task, dataset in self.processed_tasks.items()
+        }
+        # We revents trainer from automatically evaluating on each dataset:
+        # transformerS.Trainer recognizes eval_dataset instances of "dict"
+        # But we use a custom "evaluate" function so that we can use different metrics for each task
+        self.eval_dataset = MappingProxyType(self.eval_dataset)
+
+    @staticmethod
+    def write_line(other, values):
+        if other.inner_table is None:
+            other.inner_table = [list(values.keys()), list(values.values())]
+        else:
+            columns = other.inner_table[0]
+            for key in values.keys():
+                if key not in columns:
+                    columns.append(key)
+            other.inner_table[0] = columns
+            other.inner_table.append([values.get(c, np.nan) for c in columns])
+
+    def evaluate(self, **kwargs):
+        self.callback_handler.callbacks[-1].training_tracker.write_line = fc.partial(
+            self.write_line, self.callback_handler.callbacks[-1].training_tracker
+        )
+        outputs = []
+        for i, task in enumerate(self.tasks):
+            self.compute_metrics = task.compute_metrics
+            output = transformers.Trainer.evaluate(
+                self,
+                eval_dataset=dict([fc.nth(i, self.eval_dataset.items())]),
+            )
+            if "Accuracy" not in output:
+                output["Accuracy"] = np.nan
+            outputs += [output]
+        return fc.join(outputs)
+
+    def get_single_train_dataloader(self, task_name, train_dataset):
+        """
+        Create a single-task data loader that also yields task names
+        """
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+        train_sampler = (
+            RandomSampler(train_dataset)
+            if self.args.local_rank == -1
+            else DistributedSampler(train_dataset)
+        )
+
+        data_loader = DataLoaderWithTaskname(
+            task_name=task_name,
+            data_loader=DataLoader(
+                train_dataset,
+                batch_size=self.args.train_batch_size,
+                sampler=train_sampler,
+                collate_fn=self.data_collator.__call__,
+            ),
+        )
+
+        return data_loader
+
+    def get_train_dataloader(self):
+        """
+        Returns a MultitaskDataloader, which is not actually a Dataloader
+        but an iterable that returns a generator that samples from each
+        task Dataloader
+        """
+        return MultitaskDataloader(
+            {
+                task_name: self.get_single_train_dataloader(task_name, task_dataset)
+                for task_name, task_dataset in self.train_dataset.items()
+            }
+        )
+
+    def get_eval_dataloader(self, eval_dataset=None):
+        return MultitaskDataloader(
+            {
+                task_name: self.get_single_train_dataloader(task_name, task_dataset)
+                for task_name, task_dataset in (
+                    eval_dataset if eval_dataset else self.eval_dataset
+                ).items()
+            }
+        )
+
+
+def preprocess_tasks(tasks, tokenizer):
+    def add_task(x, i=None):
+        x["task"] = i
+        return x
+
+    for i, task in enumerate(tasks):
+        task.index = i
+        for split in task.dataset:
+            task.dataset[split] = task.dataset[split].map(add_task, fn_kwargs={"i": i})
+            task.index = task.dataset[split].index = i
+    tasks = copy.deepcopy(tasks)
+    features_dict = {}
+    for i, task in enumerate(tasks):
+
+        task.tokenizer = tokenizer
+        if hasattr(task, "y") and task.y != "labels":
+            task.dataset = task.dataset.rename_column(task.y, "labels")
+        features_dict[task] = {}
+        for phase, phase_dataset in task.dataset.items():
+            phase_dataset.index = i
+            features_dict[task][phase] = phase_dataset.map(
+                task.preprocess_function, batched=True, load_from_cache_file=False
+            )
+            features_dict[task][phase].set_format(
+                type="torch", columns=["input_ids", "attention_mask", "labels", "task"]
+            )
+    return features_dict
--- src/tasks.py
+++ src/tasks.py
@@ -1,1 +1,219 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import transformers
+import datasets
+from datasets import load_dataset
+from torch.utils.data.dataloader import DataLoader
+from transformers.data.data_collator import InputDataClass
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data.sampler import RandomSampler
+from typing import List, Union, Dict
+from transformers import DefaultDataCollator
+from transformers import DataCollatorForTokenClassification
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    DataCollatorForSeq2Seq,
+    AutoModelForSequenceClassification,
+    AutoModelForMultipleChoice,
+    AutoModelForTokenClassification,
+)
+from transformers import EncoderDecoderModel
+from evaluate import load as load_metric
+from lazy_load import lazy_func
+from easydict import EasyDict as edict
+import funcy as fc
+import evaluate
+import copy
+import inspect
+import functools
+from dataclasses import dataclass
+from types import MappingProxyType
 
+load_dataset = lazy_func(datasets.load_dataset)
+
+
+class Task:
+    def __hash__(self):
+        return hash(str(self.dataset.__dict__))
+
+    def __init__(self):
+        self.name = ""
+        self.__class__.__hash__ = Task.__hash__
+        if type(self.dataset) == str:
+            self.dataset = load_dataset(self.dataset)
+        if type(self.dataset) == tuple:
+            self.dataset = load_dataset(*self.dataset)
+
+
+class Classification(Task):
+    task_type = "SequenceClassification"
+    data_collator = DefaultDataCollator()
+    tokenizer_kwargs = edict(truncation=True, padding="max_length", max_length=256)
+    fields = s1, s2, y = "sentence1", "sentence2", "labels"
+    num_labels = None
+
+    def __init__(self, tokenizer):
+        super().__init__()
+        target = self.dataset["train"].features[self.y]
+        if not self.num_labels:
+            self.num_labels = 1 if "float" in target.dtype else target.num_classes
+        self.tokenizer = tokenizer
+
+    def preprocess_function(self, examples):
+        inputs = (
+            (examples[self.s1], examples[self.s2])
+            if self.s2 in examples
+            else (examples[self.s1],)
+        )
+        outputs = self.tokenizer(*inputs, **self.tokenizer_kwargs)
+        outputs["task"] = [self.index] * len(examples[fc.first(examples)])
+        return outputs
+
+    def compute_metrics(self, eval_pred):
+        predictions, labels = eval_pred.predictions, eval_pred.label_ids
+        if "int" in str(eval_pred.label_ids.dtype):
+            metric = load_metric("glue", "mnli")
+            predictions = np.argmax(predictions, axis=1)
+        else:
+            metric = load_metric("glue", "stsb")
+        meta = {"name": self.name, "size": len(predictions), "index": self.index}
+        return {**metric.compute(predictions=predictions, references=labels), **meta}
+
+
+@dataclass
+class DataCollatorForMultipleChoice:
+    tokenizer: None
+    tokenizer_kwargs: None
+
+    def __call__(self, features):
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature.pop(label_name) for feature in features]
+        tasks = [feature.pop("task") for feature in features]
+        batch_size = len(features)
+        num_choices = len(features[0]["input_ids"])
+        flattened_features = [
+            [{k: v[i] for k, v in feature.items()} for i in range(num_choices)]
+            for feature in features
+        ]
+        flattened_features = sum(flattened_features, [])
+
+        batch = self.tokenizer.pad(flattened_features, **self.tokenizer_kwargs)
+
+        # Un-flatten
+        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+        # Add back labels
+        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
+        batch["task"] = torch.tensor(tasks, dtype=torch.int64)
+        return batch
+
+
+@dataclass
+class MultipleChoice(Classification):
+    task_type = "MultipleChoice"
+    tokenizer_kwargs = edict(padding="max_length", max_length=256)
+    num_labels = 2
+
+    def __init__(self, tokenizer):
+        super().__init__(tokenizer)
+        self.tokenizer = tokenizer
+        self.data_collator = DataCollatorForMultipleChoice(
+            tokenizer=self.tokenizer, tokenizer_kwargs=self.tokenizer_kwargs
+        )
+
+    def preprocess_function(self, examples):
+        num_choices = len(self.choices)
+        num_examples = len(examples[self.s1])
+        first_sentences = [[context] * num_choices for context in examples[self.s1]]
+        second_sentences = [
+            [examples[end][i] for end in self.choices] for i in range(num_examples)
+        ]
+
+        # Flatten everything
+        first_sentences = sum(first_sentences, [])
+        second_sentences = sum(second_sentences, [])
+
+        # Tokenize
+        tokenized_examples = self.tokenizer(
+            first_sentences, second_sentences, truncation=True
+        )
+
+        # Un-flatten
+        outputs = {
+            k: [v[i : i + num_choices] for i in range(0, len(v), num_choices)]
+            for k, v in tokenized_examples.items()
+        }
+        return outputs
+
+
+class TokenClassification(Task):
+    tokenizer_kwargs = edict(truncation=True, padding="max_length", max_length=256)
+    task_type = "TokenClassification"
+    metric = evaluate.load("seqeval")
+
+    @staticmethod
+    def align_labels_with_tokens(labels, word_ids):
+        new_labels = []
+        current_word = None
+        for word_id in word_ids:
+            if word_id != current_word:
+                # Start of a new word!
+                current_word = word_id
+                label = -100 if word_id is None else labels[word_id]
+                new_labels.append(label)
+            elif word_id is None:
+                # Special token
+                new_labels.append(-100)
+            else:
+                # Same word as previous token
+                label = labels[word_id]
+                # If the label is B-XXX we change it to I-XXX
+                if label % 2 == 1:
+                    label += 1
+                new_labels.append(label)
+        return new_labels
+
+    def __init__(self, tokenizer):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.tokenizer.add_prefix_space = True
+        self.data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+        target = self.dataset["train"].features[self.y]
+        self.num_labels = 1 if "float" in target.dtype else target.feature.num_classes
+        self.label_names = [f"{i}" for i in range(self.num_labels)]
+
+    def preprocess_function(self, examples):
+        tokenized_inputs = self.tokenizer(
+            examples[self.tokens], is_split_into_words=True, **self.tokenizer_kwargs
+        )
+        all_labels = examples["labels"]
+        new_labels = []
+        for i, labels in enumerate(all_labels):
+            word_ids = tokenized_inputs.word_ids(i)
+            new_labels.append(self.align_labels_with_tokens(labels, word_ids))
+        tokenized_inputs["labels"] = new_labels
+        outputs = tokenized_inputs
+        outputs["task"] = [self.index] * len(examples[fc.first(examples)])
+        return outputs
+
+    def compute_metrics(self, eval_pred):
+        logits, labels = eval_pred.predictions, eval_pred.label_ids
+        predictions = np.argmax(logits, axis=-1)
+
+        # Remove ignored index (special tokens) and convert to labels
+        true_labels = [
+            [self.label_names[l] for l in label if l != -100] for label in labels
+        ]
+        true_predictions = [
+            [self.label_names[p] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(predictions, labels)
+        ]
+        all_metrics = self.metric.compute(
+            predictions=true_predictions, references=true_labels
+        )
+        return {
+            "precision": all_metrics["overall_precision"],
+            "recall": all_metrics["overall_recall"],
+            "f1": all_metrics["overall_f1"],
+            "accuracy": all_metrics["overall_accuracy"],
+        }
