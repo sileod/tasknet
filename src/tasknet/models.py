7e0ece47622f538ec8126a4c64e72d9119bec53e
sileod <damien.sileo@gmail.com>
2023-02-20 16:12:30 +0100

    batch truncation (speed improvement)

--- src/tasknet/models.py
+++ src/tasknet/models.py
@@ -140,6 +140,12 @@ class Model(transformers.PreTrainedModel):
 
     def forward(self, task, **kwargs):
         task_index = task[0].item()
+        if self.batch_truncation:
+            batch_max_size=kwargs['attention_mask'].sum(axis=1).max().item()
+            kwargs['attention_mask']=kwargs['attention_mask'][:,:batch_max_size].contiguous() 
+            kwargs['input_ids']=kwargs['input_ids'][:,:batch_max_size].contiguous() 
+            if len(kwargs['labels'].shape)>1:
+                kwargs['labels']=kwargs['labels'][:,:batch_max_size].contiguous() 
         y = self.task_models_list[task_index](**kwargs)
         return y
 
@@ -245,6 +251,8 @@ class Trainer(transformers.Trainer):
         self.p = hparams.get('p', 1)
         self.num_proc = hparams.get('num_proc',None)
         self.batched = hparams.get('batched',False)
+        self.batch_truncation = hparams.get('batch_truncation',True)
+
         trainer_args = transformers.TrainingArguments(
             **{**default, **fc.project(hparams, dir(transformers.TrainingArguments))},
         )
